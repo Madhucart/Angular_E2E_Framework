a2eaba9773fe88f0622b8bd717a0d77f2be13095
SOGETI\mbajina <madhuri.bajina@us.sogeti.com>
2019-12-11 15:35:45 -0500

    initial commit

---
+++ e2e/config.js
@@ -0,0 +1,10 @@
+exports.config = {
+    //------Dev------
+ //url: 'http://localhost:4200/'
+//------Test------
+url: 'http://localhost:4201/'
+//------UAT------
+// url: 'http://localhost:4202/'
+//------Production------
+// url: 'http://localhost:4203/'
+};
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    test: true,
+    production: false
+  };
+  
